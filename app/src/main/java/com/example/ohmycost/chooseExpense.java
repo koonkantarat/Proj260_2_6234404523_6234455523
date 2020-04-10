283b6e3168c33719fcdb280ce4b0738520ee55dc
varisara2103 <11241varisara@gmail.com>
2020-04-10 19:26:39 +0700

    เพิ่มปุ่มกดและกล่องข้อความในหน้าchooseExpense

--- app/src/main/java/com/example/ohmycost/chooseExpense.java
+++ app/src/main/java/com/example/ohmycost/chooseExpense.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class chooseExpense extends AppCompatActivity {
+
+    private TextView typeselect;
+    private Button select, ok, back;
+    private EditText cost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_expense);
-        Button back = findViewById(R.id.backToMain);
+        typeselect = findViewById(R.id.typeselect);
+        select = findViewById(R.id.select);
+        ok = findViewById(R.id.ok);
+        cost = findViewById(R.id.cost);
+
+        back = findViewById(R.id.backToMain);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
